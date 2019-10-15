f9ac89369e95381d74d24ea6fb4ebaf1cae4c01f
vienpn <vien.phamngoc@gmail.com>
2019-10-15 17:51:50 +0700

    init training task-2

---
+++ Training4/task2/src/App.js
@@ -0,0 +1,27 @@
+import React, { Comment } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+export default class App extends Comment{
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
+};
+
