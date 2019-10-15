d525a07f6552d583a2fee1d5cf8dd637b9ff7a9f
vienpn <vien.phamngoc@gmail.com>
2019-10-15 17:58:27 +0700

    init app

--- Training4/task2/src/App.js
+++ Training4/task2/src/App.js
@@ -1,24 +1,30 @@
-import React, { Comment } from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-export default class App extends Comment{
+export default class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      dataItems: [],
+      error: '',
+      isLoaded: false,
+    }
+  }
+  countriesAPI = () => {
+    console.log('hi hi ')
+  }
+  componentDidMount(){
+    this.countriesAPI();
+  }
   render(){
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <img src={logo} className="App-logo" alt="logo"  style={{width: '160px' }}/>
+          <div>
+
+          </div>
         </header>
       </div>
     );
